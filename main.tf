692af4fe1ce99517b9a185e2a7e8ba34a2e4cae5
Najah-Ahmed <najaah8800@gmail.com>
2020-09-13 20:40:32 +0300

    change was made

--- main.tf
+++ main.tf
@@ -5,13 +5,16 @@ provider "google" {
   zone        = "us-central1-c"
 }
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance-njh"
+  name         = "terraform-instance-njh2"
   machine_type = "f1-micro"
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
     }
+  }
+  metadata = {
+    ssh-keys = "INSERT_USERNAME:${file("~/.ssh/id_rsa_gcp.pub")}"
   }
 
   network_interface {
@@ -20,4 +23,13 @@ resource "google_compute_instance" "vm_instance" {
     access_config {
     }
   }
+  # output "ip" {
+  #   value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+  # }
+  # allow {
+  #   protocol = "tcp"
+  #   ports    = ["5000"]
+  # }
+
 }
+
